639f03bdd57a180a3dab66ae5d7dd148091a18ad
SathyaSelvam <sdssathya31@gmail.com>
2016-08-22 14:40:38 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.Scanner;
+
+public class Conversion
+{
+	public static void main(String[] args) 
+	{
+		String s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		 int i=Integer.parseInt(s);
+		 
+		    System.out.println(i);
+	}
+ 
+}
